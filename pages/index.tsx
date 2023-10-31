cec8ba0f45ac671b1298532b54910daaebf49a64
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-10-30 23:11:13 -0300

    ft: add trending movie list to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import useMovieList from "@/hooks/useMovieList";
+
 
 export async  function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,10 +24,16 @@ export async  function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar/>
       <Billboard/>
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
