5d6a4599bb2fd2844361fb79728aa9c8d757b2e4
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-11-11 21:27:49 -0300

    ft: add info modal to movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,7 +3,9 @@ import { useRouter } from "next/router";
 
 
 import { BsFillPlayFill } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
+import useInfoModal from "@/hooks/useInfoModal";
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -13,6 +15,7 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({data}) => {
 
     const router = useRouter();
+    const { openModal } = useInfoModal();
 
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -82,6 +85,9 @@ const MovieCard: React.FC<MovieCardProps> = ({data}) => {
                             <BsFillPlayFill size={20} />
                         </div>
                         <FavoriteButton movieId={data?.id} />
+                        <button onClick={() => {openModal(data?.id)}} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+                            <BiChevronDown size={30} className="text-white group-hover/tem:text-neutral-300"/>
+                        </button>
                     </div>
 
                     <p className="text-green-400 font-semibold mt-4">
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import useMovieList from "@/hooks/useMovieList";
 import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal from "@/hooks/useInfoModal";
 
 
 export async  function getServerSideProps(context: NextPageContext) {
@@ -28,10 +30,12 @@ export default function Home() {
 
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar/>
       <Billboard/>
       <div className="pb-40">
